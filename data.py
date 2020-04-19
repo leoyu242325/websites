d38cbd7939dc2e76258a3645dd5731a92c8564fc
leoyu242325 <63951048+leoyu242325@users.noreply.github.com>
2020-04-19 17:12:10 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id':1,
+			'title':'Artitle One',
+			'body':'~~~~~~~~~~~~',
+			'author':'~~~~~~~~~',
+			'create_date':'04-25-2018'
+		},
+		{
+			'id':2,
+			'title':'two',
+			'body':'~~~~~~~~~~~~',
+			'author':'~~~~~~~~~',
+			'create_date':'04-25-2018'
+		},
+		{
+			'id':3,
+			'title':'three',
+			'body':'~~~~~~~~~~~~',
+			'author':'~~~~~~~~~',
+			'create_date':'04-25-2018'
+		},
+	]
+	return articles 
